35360f3bff4bb81cac39959e136b1231d1065412
eelisabeth <eelisabeth.pro@gmail.com>
2022-05-28 17:14:01 +0200

    create onDrag events & useRef

--- src/components/drop-file-input/DropFileInput.js
+++ src/components/drop-file-input/DropFileInput.js
@@ -1,10 +1,24 @@
 import PropTypes from 'prop-types';
 import './drop-file-input.css';
 import uploadImg from '../../assets/cloud-upload-regular-240.png';
+import { useRef } from 'react';
 
 const DropFileInput = props => {
+
+    const wrapperRef = useRef(null);
+
+    const onDragEnter = () => wrapperRef.current.classList.add('dragover');
+    const onDragLeave = () => wrapperRef.current.classList.remove('dragover');
+    const onDrop = () => wrapperRef.current.classList.remove('dragover');
+
     return (
-        <div className="drop-file-input">
+        <div
+            ref={wrapperRef}
+            className="drop-file-input"
+            onDragEnter={onDragEnter}
+            onDragLeave={onDragLeave}
+            onDrop={onDrop}
+        >
             <div className="drop-file-input__label">
                 <img src={uploadImg} alt="" />
                 <p>Drag & Drop your files here</p>
