5351b8075e17abc695a14f162cccf3df7d115843
eelisabeth <eelisabeth.pro@gmail.com>
2022-05-28 16:37:34 +0200

    add global css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <div className="box">
+			<h2 className="header">
+				React drop files input
+			</h2>
+        </div>
+    );
 }
 
 export default App;
